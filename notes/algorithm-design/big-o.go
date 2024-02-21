2515ee7290202fa600b29840c3e3b6e81783a8c9
Aseem Chopra <aseemchopra@protonmail.com>
2024-02-21 14:59:45 +0530

    add: 2-big-o

--- notes/algorithm-design/big-o.go
+++ notes/algorithm-design/big-o.go
@@ -11,6 +11,18 @@ func product(a int, b int) int {
 	return sum
 }
 
+func atopowerb(a int, b int) int {
+	if b < 0 {
+		return 0
+	} else if b == 0 {
+		return 1
+	} else {
+		return a * atopowerb(a, b-1)
+	}
+
+}
+
 func main() {
 	fmt.Println(product(10, 20)) // Big-O: O(b)
+	fmt.Println(atopowerb(5, 2)) // Big-O: O(b)
 }
