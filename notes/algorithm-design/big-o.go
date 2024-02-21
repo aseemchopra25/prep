18a813159a70c362611c38d8656be0589ab11600
Aseem Chopra <aseemchopra@protonmail.com>
2024-02-21 15:21:03 +0530

    add: 4-big-o

--- notes/algorithm-design/big-o.go
+++ notes/algorithm-design/big-o.go
@@ -34,8 +34,22 @@ func mod(a int, b int) int {
 	return a - div*b
 }
 
+// 4.
+func div(a int, b int) int {
+	count := 0
+	sum := b
+	for sum <= a {
+		sum += b
+		count++
+	}
+	return count
+
+}
+
 func main() {
 	fmt.Println(product(10, 20)) // Big-O: O(b)
 	fmt.Println(atopowerb(5, 2)) // Big-O: O(b)
 	fmt.Println(mod(10, 3))      // Big-O: O(1)
+	fmt.Println(div(10, 3))      // Big-O: O(a/b)
+
 }
